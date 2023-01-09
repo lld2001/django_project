24d9138e475a9abb5ca473f2a425fef7fb87da33
lld2001h <1353647436@qq.com>
2023-01-09 21:17:51 +0800

    init

---
+++ accounts/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+# accounts/views.py
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+
+from .forms import CustomUserCreationForm
+
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
